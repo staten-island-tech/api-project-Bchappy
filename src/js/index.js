5e66e99448abc24478555e6aea1130613754711f
B-Chappy <71845545+B-Chappy@users.noreply.github.com>
2021-05-11 16:34:52 -0400

    Fetch works!

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,33 @@ const query = async function(){
         );
         const data = await response.json();
         console.log(data.results.books);
+        data.results.books.forEach((book) =>{
+            DOMSelectors.grid.insertAdjacentHTML("beforeend", `<div class="movie-card">
+            <div class="movie-card-front">
+              <img
+                src=${book.book_image}
+                alt=""
+                class="poster"
+              />
+            </div>
+            <div class="movie-card-back">
+              <h3 class="movie-card-header">${book.title}</h3>
+              <div class="score-box">
+                <p class="user-score">Best Seller Ranking</p>
+                <p class="user-score">#${book.rank}</p>
+              </div>
+    
+              <div class="release-box">
+                <p class="release-date">Released</p>
+                <p class="release-date">2020-06-12</p>
+              </div>
+    
+              <div class="movie-genres">
+                <li class="movie-genre">See on Amazon</li>
+              </div>
+            </div>
+          </div>`)
+        })
     }
     catch (error) {
         alert("An error occured. Please try again.");
