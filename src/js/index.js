50f4ac2f5490f5e77817d12d96da098f2db7475b
B-Chappy <71845545+B-Chappy@users.noreply.github.com>
2021-05-11 09:58:36 -0400

    New API with book ratings

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "i7NBVLUt7zb2aigI2OkdIsBQjUXIOpJy";
+const query = async function(){
+    try{
+        const response = await fetch(
+            `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${key}`
+        );
+        const data = await response.json();
+        console.log(data.results.books);
+    }
+    catch (error) {
+        alert("An error occured. Please try again.");
+    }
+}
+query();
