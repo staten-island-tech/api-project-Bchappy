97ae5314936ffa38beb7989f756228be8ff7f92c
B-Chappy <71845545+B-Chappy@users.noreply.github.com>
2021-05-13 20:52:27 -0400

    Small improvements + navbar

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ const key = "i7NBVLUt7zb2aigI2OkdIsBQjUXIOpJy";
 const query = async function(){
     try{
         const response = await fetch(
-            `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-nonfiction.json?api-key=${key}`
+            `https://api.nytimes.com/svc/books/v3/lists/current/young-adult-hardcover.json?api-key=${key}`
         );
         const data = await response.json();
         console.log(data.results.books);
@@ -20,19 +20,20 @@ const query = async function(){
             </div>
             <div class="movie-card-back">
               <h3 class="movie-card-header">${book.title}</h3>
+              <div class="release-box">
+                <p class="release-date">${book.author}</p>
+                <p class="release-date"></p>
+              </div>
               <div class="score-box">
                 <p class="user-score">Best Seller Ranking</p>
                 <p class="user-score">#${book.rank}</p>
               </div>
     
-              <div class="release-box">
-                <p class="release-date">Released</p>
-                <p class="release-date">2020-06-12</p>
-              </div>
+              
     
-              <div class="movie-genres">
-                <a href="${book.amazon_product_url}" class="movie-genre">See on Amazon</a>
-              </div>
+              
+              <a href="${book.amazon_product_url}" class="movie-genre">See on Amazon</a>
+            
             </div>
           </div>`)
         })
